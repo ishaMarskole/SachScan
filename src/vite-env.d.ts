d7a926b43169dc9b7c77b4877dbc60801b502920
ishaMarskole <aarohipardhan890@gmail.com>
2026-05-05 16:27:16 +0530

    third commit

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_OPENAI_KEY?: string
+  readonly VITE_GEMINI_KEY?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
